c0a177c0b774410c095ef30913596361a1ff7f50
Ann - Woravan <woravan.suthatar@rockyou.com>
2019-03-29 14:50:24 +0700

    update template

---
+++ src/components/PageHome.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const Wrapper = styled.div``
+
+const PageHome = props => {
+  return <Wrapper>PageHome</Wrapper>
+}
+
+PageHome.propTypes = {}
+
+PageHome.defaultProps = {}
+
+export default PageHome
---
+++ src/pages/home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import HomeComponent from '../components/PageHome'
+
+const Homepage = () => {
+  return <HomeComponent />
+}
+
+export default Homepage
